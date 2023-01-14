b33ec75796ee394dc52bc3b7b0270df8e6cfd031
Jzakka <mouse4786@gmail.com>
2023-01-14 23:35:48 +0900

    feat: 사용자 삭제 & 수정 기능 추가

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,12 @@ const User = require('../models/user');
 module.exports.getUsers = async (req, res, next) => {
     const email = req.query.email;
     const username = req.query.username;
-    const where ={};
+    const where = {};
 
-    if(email){
+    if (email) {
         where.email = email;
     }
-    if(username){
+    if (username) {
         where.username = username;
     }
     const users = await User.findAll({
@@ -18,23 +18,25 @@ module.exports.getUsers = async (req, res, next) => {
     return res.status(200).json(users);
 };
 
-module.exports.getUser = async (req,res,next) => {
+module.exports.getUser = async (req, res, next) => {
     const userId = req.params.userId;
 
     const user = await User.findByPk(userId);
 
-    if(!user){
-        return res.status(404);
+    if (!user) {
+        // console.log('Not found');
+        return res.status(404).json({ Error: 'No such user' });
     }
 
+    // console.log('Found');
     return res.status(200).json(user);
-}
+};
 
-module.exports.addUser = async (req,res,next)=>{
+module.exports.addUser = async (req, res, next) => {
     const email = req.body.email;
     const username = req.body.username;
     const password = req.body.password;
-    
+
     const newUser = await User.create({
         username: username,
         email: email,
@@ -42,4 +44,56 @@ module.exports.addUser = async (req,res,next)=>{
     });
 
     return res.status(200).json(newUser);
-}; 
+};
+
+module.exports.deleteUser = async (req, res, next) => {
+    const userId = req.params.userId;
+
+    return await User
+        .destroy({
+            where: { id: userId }
+        })
+        .then(result => {
+            console.log(result);
+            if (!result) throw new Error('Delete failed');
+            return res.status(200).json({ Message: `user ${userId} was deleted` });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(404).json({ Error: 'No such user' });
+        });
+};
+
+module.exports.updateUser = async (req, res, next) => {
+    const userId = req.params.userId;
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const updatedUserInfo = {};
+    if (email) {
+        updatedUserInfo.email = email;
+    }
+    if (username) {
+        updatedUserInfo.username = username;
+    }
+    if (password) {
+        updatedUserInfo.password = password;
+    }
+
+    return await User
+        .update(updatedUserInfo, {
+            where: {
+                id: userId
+            }
+        })
+        .then(result => {
+            console.log(result);
+            if (!result) throw new Error('Update failed');
+            return res.status(200).json({ Message: `user ${userId} was updated` });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(404).json({ Error: 'No such user' });
+        });
+};
