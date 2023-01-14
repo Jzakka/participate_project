5d723e6db202761776111540d8405d522f76c78b
Jzakka <mouse4786@gmail.com>
2023-01-14 23:41:24 +0900

    fix: 업데이트 실패 케이스 조건 수정

--- __tests__/models/user.js
+++ __tests__/models/user.js
@@ -12,6 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/users', userController.addUser);
 app.get('/users/:userId', userController.getUser);
 app.get('/users', userController.getUsers);
+app.delete('/users/:userId', userController.deleteUser);
+app.put('/users/:userId', userController.updateUser);
 
 beforeEach(async () => {
     association();
@@ -98,6 +100,112 @@ describe('UserTest', () => {
                 assert.strictEqual(res.body.email, 'test@test.com');
                 assert.strictEqual(res.body.username, 'testuser');
                 assert.strictEqual(res.body.password, '1234');
+            });
+    });
+
+    test('getUser-failed', async () => {
+        const res = await request(app)
+            .get('/users/' + 250)
+            .set('Accept', 'application/json')
+            .type('application/json');
+
+        // console.log(res);
+        expect(res.status).toEqual(404);
+    });
+
+    test('deleteUser-success', async () => {
+        let userId;
+        await request(app)
+            .post('/users')
+            .set('Accept', 'application/json')
+            .type('application/json')
+            .send({
+                email: 'test@test.com',
+                username: 'testuser',
+                password: '1234'
+            })
+            .then(res => {
+                userId = res.body.id;
+            });
+        await request(app)
+            .delete('/users/' + userId)
+            .expect(200)
+            .then(res => {
+                assert.equal(res.body.Message, `user ${userId} was deleted`);
+            });
+    });
+
+    test('deleteUser-failed', async () => {
+        await request(app)
+            .post('/users')
+            .set('Accept', 'application/json')
+            .type('application/json')
+            .send({
+                email: 'test@test.com',
+                username: 'testuser',
+                password: '1234'
+            })
+        await request(app)
+            .delete('/users/' + 404)
+            .expect(404)
+            .then(res => {
+                assert.equal(res.body.Error, 'No such user');
+            });
+    });
+
+    test('updateUser-success', async () => {
+        let userId;
+        await request(app)
+            .post('/users')
+            .set('Accept', 'application/json')
+            .type('application/json')
+            .send({
+                email: 'test@test.com',
+                username: 'testuser',
+                password: '1234'
+            })
+            .then(res => {
+                userId = res.body.id;
+            });
+        await request(app)
+            .put('/users/' + userId)
+            .set('Accept', 'application/json')
+            .type('application/json')
+            .send({
+                username: 'pikachu',
+                password: '9999'
+            })
+            .expect(200)
+            .then(res => {
+                assert.equal(res.body.Message, `user ${userId} was updated`);
+            });
+        await request(app)
+            .get('/users/' + userId)
+            .expect(200)
+            .then(res => {
+                assert.deepStrictEqual([
+                        res.body.email,
+                        res.body.username,
+                        res.body.password], [
+                        'test@test.com',
+                        'pikachu',
+                        '9999'
+                    ]);
+            });
+    });
+
+    test('updateUser-failed', async () => {
+        await request(app)
+            .put('/users/' + 404)
+            .set('Accept', 'application/json')
+            .type('application/json')
+            .send({
+                username: 'pikachu',
+                password: '9999'
+            })
+            .expect(404)
+            .then(res => {
+                assert.equal(res.body.Error, 'No such user');
             });
     });
 });
