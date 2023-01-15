7820b1fa1027c93ef087491053df9422dbf51d2e
Jzakka <mouse4786@gmail.com>
2023-01-15 12:55:35 +0900

    test: 게시판 조회&추기

---
+++ __tests__/models/board.js
@@ -0,0 +1,77 @@
+const request = require('supertest');
+const assert = require('assert');
+const express = require('express');
+const bodyParser = require('body-parser');
+const association = require('../../models/association/association');
+const sequelize = require('../../database/in-memory');
+const boardController = require('../../controllers/boardController');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/boards', boardController.getBoards);
+app.post('/boards', boardController.addBoard);
+app.delete('/boards/:boardId', boardController.deleteBoard);
+
+beforeEach(async () => {
+    association();
+    return await sequelize
+        .sync({ force: true })
+        .then(() => {
+            console.log('Model Sync Complete');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+describe('BoardTest', () => {
+    test('addBoard', async () => {
+        await request(app)
+            .post('/boards')
+            .set('Accept', 'application/json')
+            .type('application/json')
+            .send({
+                boardName: 'NewBoard'
+            })
+            .expect(200)
+            .then(result => {
+                assert.strictEqual(result.body.boardName, 'NewBoard');
+            });
+    });
+    test('getBoards', async () => {
+        await request(app)
+            .post('/boards')
+            .set('Accept', 'application/json')
+            .type('application/json')
+            .send({
+                boardName: 'NewBoard'
+            });
+        await request(app)
+            .get('/boards?board_name=NewBoard')
+            .expect(200)
+            .then(result => {
+                assert.strictEqual(result.body[0].boardName, 'NewBoard');
+                assert.notStrictEqual(result.body[0].boardName, 'OldBoard');
+            });
+    });
+    test('deleteBoard-success', async () => {
+        let boardId;
+        await request(app)
+            .post('/boards')
+            .set('Accept', 'application/json')
+            .type('application/json')
+            .send({
+                boardName: 'NewBoard'
+            })
+            .then(result => {
+                boardId = result.id;
+            });
+        await request(app)
+            .delete('/boards/'+boardId)
+            .expect(200)
+            .then(result => {
+                assert.deepStrictEqual(result.body, {Message:  'Deleted Successfull'});
+            });
+    });
+});
