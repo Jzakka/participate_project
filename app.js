89e9a8542abb62e13b670734ef2b26e591c09878
Jzakka <mouse4786@gmail.com>
2023-01-17 22:59:13 +0900

    feat: 라우터 추가

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
 const boardRouter = require('./router/boardRouter');
 const userRouter = require('./router/userRouter');
 const postRouter = require('./router/postRouter');
+const loginRouter = require('./router/loginRouter');
 
 const app = express();
+
+app.use(
+    session({
+        secret: 'victoria secret',
+        resave: false,
+        saveUninitialized: true,
+        store: new MemoryStore({
+            checkPeriod: 3600
+        }),
+        cookie: {maxAge: 3600}
+    })
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(loginRouter);
 app.use('/users',userRouter);
 app.use('/boards', boardRouter);
 app.use('/posts', postRouter);
