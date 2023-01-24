0370616ebcacf8effd1b787280308b571bcc636f
Jzakka <mouse4786@gmail.com>
2023-01-24 16:47:22 +0900

    feat: 사용자&게시판 태그 추가

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -10,25 +10,25 @@ module.exports.getTags = async (req, res, next) => {
 
     const query = {
         // logging: true,
-        include:[ {
+        include: [{
             model: User,
             attributes: ['id'],
-        },{
+        }, {
             model: Board,
             attributes: ['id'],
-        },{
+        }, {
             model: Post,
             attributes: ['id'],
         }],
     };
-    if(userId){
-        query.include[0].where = {id: userId};
+    if (userId) {
+        query.include[0].where = { id: userId };
     }
-    if(boardId){
-        query.include[1].where = {id: boardId};
+    if (boardId) {
+        query.include[1].where = { id: boardId };
     }
-    if(postId){
-        query.include[2].where = {id: postId};
+    if (postId) {
+        query.include[2].where = { id: postId };
     }
 
     return await Tag
@@ -38,22 +38,46 @@ module.exports.getTags = async (req, res, next) => {
         })
         .catch(err => {
             console.log(err);
-            return res.status(400).json({message: "An error occured"});
+            return res.status(400).json({ message: "An error occured" });
         });
 };
 
-module.exports.addTag = async (req,res,next)=>{
-    const tagName =  req.body.tagName; 
+module.exports.addTag = async (req, res, next) => {
+    const tagName = req.body.tagName;
+    const userId = req.body.userId;
+    const boardId = req.body.boardId;
+
+
 
     return await Tag
         .create({
             tagName: tagName
         })
+        .then(async found => {
+            if (userId) {
+                await User
+                    .findByPk(userId)
+                    .then(foundUser =>{
+                        foundUser.addTag(found);
+                    });
+            }
+            return found;
+        })
+        .then(async found => {
+            if (userId) {
+                await Board
+                    .findByPk(boardId, {include: Tag})
+                    .then(foundBoard =>{
+                        foundBoard.addTag(found);
+                    });
+            }
+            return found;
+        })
         .then(result => {
-            return res.status(200).json({message: 'Created Tag successfull'});
+            return res.status(200).json({ message: 'Created Tag successfull' });
         })
         .catch(err => {
             console.log(err);
-            return res.status(404).json({message: "Failed to create Tag"});
+            return res.status(404).json({ message: "Failed to create Tag" });
         })
 };
