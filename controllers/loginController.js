f710538e0485f89006caed17ec5b365b44aeae80
Jzakka <mouse4786@gmail.com>
2023-01-27 14:17:27 +0900

    feat: 토큰 방식 로그인

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,37 +1,52 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
 const User = require('../models/user');
 
-module.exports.postLogin = async (req, res, next)=>{
+module.exports.postLogin = async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    return await User.findOne({where: {
-        email: email
-    }})
-    .then(foundUser => {
-        if(!foundUser){
-            throw new Error('No such user');
+    let loadedUser;
+    return await User.findOne({
+        where: {
+            email: email
         }
-        return bcrypt
-            .compare(password, foundUser.password)
-            .then(isMatch => {
-                if(!isMatch){
-                    throw new Error('Incorrect password');
-                }
-                req.session.user = foundUser;
-                res.redirect('/');
-            });
     })
-    .catch(err => {
-        console.log(err);
-        return res.status(422).json({message: err});
-    });
+        .then(foundUser => {
+            if (!foundUser) {
+                const error = new Error('No such user');
+                error.statusCode = 401;
+                throw error;
+            }
+            loadedUser = foundUser;
+            return bcrypt.compare(password, foundUser.password);
+        })
+        .then(isMatch => {
+            if (!isMatch) {
+                const error = new Error('Password is incorrect');
+                error.statusCode = 401;
+                throw error;
+            }
+            const token = jwt.sign({
+                email: loadedUser.email,
+                username: loadedUser.username,
+                userId: loadedUser.id
+            }, process.env.TOKEN_KEY,
+                { expiresIn: '1h' });
+            return res.status(200).json({token: token, userId: loadedUser.id});
+        })
+        .catch(err => {
+            err.statusCode = err.statusCode || 500;
+            next(err);
+        });
 };
 
-module.exports.postLogout = async (req,res, next)=>{
+module.exports.postLogout = async (req, res, next) => {
     req.session.destroy(err => {
-        if(err){
-            return res.status(400).json({message: 'Failed to logout'})
+        if (err) {
+            return res.status(400).json({ message: 'Failed to logout' })
         }
         res.redirect('/');
     });
