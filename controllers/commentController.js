97747bcbf7e92b36e976c559133ea89af2d4774e
Jzakka <mouse4786@gmail.com>
2023-01-27 18:49:37 +0900

    refactor: 에러플로우 수정

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -18,40 +18,53 @@ module.exports.addComment = async (req, res, next) => {
         context: context,
         deleted: 'N'
     };
-    if (await User.findByPk(userId) && await Post.findByPk(postId)) {
-        let parent;
-        if (commentId) {
-            parent = await Comment.findByPk(commentId);
-        }
-        return await Comment
-            .create(query)
-            .then(async result => {
-                // console.log(result.getValuesDedup());
-                if (!result) {
-                    return res.status(400).json({ message: 'Failed to create comment' });
-                }
-                if (parent) {
-                    await parent.addComment(result)
-                        .then(async addedResult => {
-                            // console.log(addedResult);
-                            // console.log(Sequelize.getValues((await parent.getComments())));
-                        });
-                }
-                return res.status(201).json({
-                    CommentId: result.getValuesDedup().id,
-                    message: 'Created comment successfull'
-                });
-            })
+    if (!(await User.findByPk(userId) && await Post.findByPk(postId))) {
+        const err = new Error('Could not find user and post');
+        err.statusCode = 404;
+        throw err;
     }
-    return res.status(400).json({ message: 'Failed to create comment' });
+    let createdComment;
+    return Comment
+        .create(query)
+        .then(async result => {
+            if (!result) {
+                const err = new Error('Failed to create comment');
+                throw err;
+            }
+            createdComment = result;
+        })
+        .then(result => {
+            if (commentId) {
+                Comment
+                    .findByPk(commentId)
+                    .then(parent => {
+                        if (!parent) {
+                            const err = new Error('No such comment');
+                            err.statusCode = 404;
+                            throw err;
+                        }
+                        parent.addComment(createdComment);
+                    })
+            }
+        })
+        .then(result => {
+            res.status(201).json({
+                CommentId: createdComment.getValuesDedup().id,
+                message: 'Created comment successfull'
+            });
+        })
+        .catch(err => {
+            err.statusCode = err.statusCode || 500;
+            next(err);
+        });
 };
 
 module.exports.getComments = async (req, res, next) => {
     const postId = req.query.postId;
     const userId = req.query.userId;
     const commentId = req.query.commentId;
-    const pageNumber = req.query.pageNumber ? req.query.pageNumber : 0;
-    const pageSize = req.query.pageSize ? req.query.pageSize : 10;
+    const pageNumber = req.query.pageNumber ?? 0;
+    const pageSize = req.query.pageSize ?? 10;
     const where = {};
     if (postId) {
         where.PostId = postId;
@@ -76,56 +89,65 @@ module.exports.getComments = async (req, res, next) => {
             return res.status(200).json(Sequelize.getValues(result));
         })
         .catch(err => {
-            console.log(err);
-            return res.status(400).json({
-                message: 'An error occured'
-            });
+            err.statusCode = err.statusCode || 500;
+            next(err);
         });
 };
 
 module.exports.getComment = async (req, res, next) => {
-    const commentId  = req.params.commentId;
+    const commentId = req.params.commentId;
 
     return await Comment
-        .findByPk(commentId,{
+        .findByPk(commentId, {
             include: Comment
         })
         .then(result => {
-            if(!result) throw new Error();
+            if (!result) {
+                const err = new Error('No such comment');
+                err.statusCode = 404;
+                throw err;
+            }
             return res.status(200).json(result.getValues());
         })
-        .catch(err=>res.status(404).json({message: 'No such comment'}));
+        .catch(err => {
+            err.statusCode = err.statusCode || 500;
+            next(err);
+        });
 };
 
-module.exports.updateComment = async (req,res,next)=>{
+module.exports.updateComment = async (req, res, next) => {
     const commentId = req.params.commentId;
     const context = req.body.context;
     const deleted = req.body.deleted;
 
     const query = {};
-    if(context){
+    if (context) {
         query.context = context;
     }
-    if(deleted){
+    if (deleted) {
         query.deleted = deleted;
     }
 
-    return await Comment
-        .update(query, {
-            where:{
-                id:commentId
+    Comment.findByPk(commentId)
+        .then(result => {
+            if (!result) {
+                const err = new Error('No such commnet');
+                err.statusCode = 404;
+                throw err;
             }
+            return Comment.update(query, { where: { id: commentId } });
         })
         .then(result => {
-            if(!result) throw new Error();
+            if (!result[0]) {
+                const err = new Error('Update failed');
+                throw err;
+            }
             return res.status(200).json({
                 message: 'Updated comment successfull'
             });
         })
-        .catch(err=>{
-            console.log(err);
-            return res.status(400).json({
-                message: 'Failed to update comment'
-            });
+        .catch(err => {
+            err.statusCode = err.statusCode || 500;
+            next(err);
         });
 };
