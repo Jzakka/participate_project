b4530c999d50926e2ce99453348aa8870551d211
Jzakka <mouse4786@gmail.com>
2023-01-30 23:58:50 +0900

    refactor: deprecate된 파일에서 변경

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -1,4 +1,4 @@
-const {validationResult} = require('express-validator/check');
+const {validationResult} = require('express-validator');
 require('dotenv').config();
 
 const Board = require('../models/board');
