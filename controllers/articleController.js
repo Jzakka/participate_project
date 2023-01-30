a0592bfe2a4c8ef37bb01dc9e5fea6aebca61eb6
Jzakka <mouse4786@gmail.com>
2023-01-31 00:45:23 +0900

    fix: 사용자 태그가 없는 경우 처리

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,13 +1,12 @@
+const _ = require('lodash');
 const NewsAPI = require('newsapi');
 const User = require('../models/user');
 const Tag = require('../models/tag');
-const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
 
 module.exports.getArticles = async (req, res, next) => {
-
     if (!req.userId) {
         return newsapi.v2
             .topHeadlines()
@@ -27,7 +26,10 @@ module.exports.getArticles = async (req, res, next) => {
                     err.statusCode = 404;
                     throw err;
                 }
-                return newsapi.v2.topHeadlines({ q: foundUser.Tags.map(tag => tag.tagName) });
+                if (_.isEmpty(foundUser.Tags)) {
+                    return newsapi.v2.topHeadlines();
+                }
+                return newsapi.v2.topHeadlines({q: foundUser.Tags.map(tag=>tag.tagName)});
             })
             .then(articles => {
                 return res.status(200).json(articles);
